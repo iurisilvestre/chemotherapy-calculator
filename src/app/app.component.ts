61938708250cfe7ff900063960b8b042aba96c97
Iuri <iurisdesign@gmail.com>
2021-03-11 17:42:01 +0000

    add crcl calculation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  bsaValue = 0;
-  paclitaxel = 80;
+  bsaValue: number = 0;
+  paclitaxel: number = 80;
+  crCl: number = 0;
 
   patientInfo = {
-    genre: String,
+    genre: '',
     age: Number,
     height: Number,
     weight: Number,
@@ -24,6 +25,25 @@ export class AppComponent {
     let bsa = Math.round(0.007184 * weight * height * 10) / 10;
     this.bsaValue = bsa;
     return `${bsa} m2`;
+  }
+
+  setGenre(genre: string) {
+    this.patientInfo.genre = genre;
+  }
+
+  getCrCl(obj: any) {
+    let crClMale = ((140 - obj.age) * obj.weight) / (obj.creatinine * 72);
+    if (obj.genre === 'female') {
+      return `${Math.round((crClMale * 0.85 * 10) / 10)} mL/min`;
+    } else if (obj.genre === 'male') {
+      return `${Math.round(crClMale * 10) / 10} mL/min`;
+    } else {
+      return 'NaN';
+    }
+  }
+
+  console() {
+    console.log(this.patientInfo);
   }
 
   getPaclitaxel() {
