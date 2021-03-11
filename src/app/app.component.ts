6b3fc6c6c51034ea2c1ef067304626f81f270794
Iuri <iurisdesign@gmail.com>
2021-03-11 13:02:49 +0000

    put into one component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'chemotherapy-calculator';
+  bsaValue = 0;
+  paclitaxel = 80;
+
+  patientInfo = {
+    genre: String,
+    age: Number,
+    height: Number,
+    weight: Number,
+    creatinine: Number,
+    auc: Number,
+  };
+
+  getBsa(obj: any) {
+    let weight = Math.pow(obj.weight, 0.425);
+    let height = Math.pow(obj.height, 0.725);
+    let bsa = Math.round(0.007184 * weight * height * 10) / 10;
+    this.bsaValue = bsa;
+    return `${bsa} m2`;
+  }
+
+  getPaclitaxel() {
+    return `${Math.round(this.paclitaxel * this.bsaValue)} mg`;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { PatientInfoComponent } from './patient-info/patient-info.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PatientInfoComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
