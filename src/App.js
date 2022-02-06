38ebb8ab18af9bd56e08b4d171d48ce21f56522b
Iuri <iurisdesign@gmail.com>
2022-02-06 12:33:25 +0000

    develop reset button to work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import { CoursesSchemes } from "./CoursesSchemes.js";
+import { bSA, CrCl } from "./Formulas.js";
 
 import PatientInfo from "./components/PatientInfo";
 import SchemesSelection from "./components/SchemesSelection";
@@ -11,6 +12,8 @@ import "./App.css";
 export default function App() {
   const [patientInfo, setPatientInfo] = useState({});
   const [selectedRegimen, setSelectedRegimen] = useState("");
+  const [bsaValue, setBsaValue] = useState(0);
+  const [crclValue, setCrclValue] = useState(0);
 
   const handleInputs = (event) => {
     const { name, value, type } = event.target;
@@ -26,6 +29,20 @@ export default function App() {
     setSelectedRegimen(regimenIndex);
   };
 
+  const getBsa = () => {
+    setBsaValue(bSA(patientInfo));
+  };
+  const getCrCl = () => {
+    setCrclValue(CrCl(patientInfo));
+  };
+
+  useEffect(() => {
+    getBsa();
+    getCrCl();
+  }, [patientInfo]);
+
+  const getDose = (patientData, coursesSchemes, selectedRegimen) => {};
+
   return (
     <div className="app">
       <h1>Chemotherapy Calculator</h1>
@@ -34,7 +51,11 @@ export default function App() {
         drugsList={CoursesSchemes}
         onSelectRegiment={handleSelectRegiment}
       />
+<<<<<<< HEAD
       <Results patientInfo={patientInfo} selectedRegimen={selectedRegimen} />
+=======
+      <Results bsaValue={bsaValue} crclValue={crclValue} />
+>>>>>>> 57fe928 (crate formulas js file)
     </div>
   );
 }
